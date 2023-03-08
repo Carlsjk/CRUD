33dc9bad5751d52e0e7417deaa21f726cdd2a9e1
Jhonatan <cumb1234@gmail.com>
2023-03-08 04:53:51 -0500

    first commit

---
+++ logger_base.py
@@ -0,0 +1,21 @@
+import logging as log
+
+log.basicConfig(level=log.DEBUG,
+                format='%(asctime)s: %(levelname)s [%(filename)s:%(lineno)s] %(message)s',
+                datefmt='%I:%M:%S %p',
+                handlers=[
+                    log.FileHandler('capa_datos.log'),
+                    log.StreamHandler()
+                ])
+
+
+
+
+if __name__ == "__main__":
+    log.debug("This is a debug message")
+    log.info("This is an info message")
+    log.warning("This is a warning message")
+    log.error("This is an error message")
+    log.critical("This is a critical message")
+
+
